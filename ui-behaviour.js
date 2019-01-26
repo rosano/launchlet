664e6c1ed6716c3a67b831a7ff2c2cc92b360147
Rosano Coutinho <pub@xeari.com>
2019-01-26 17:36:06 -0500

    Scaffold Launchlet page

---
+++ ui-behaviour.js
@@ -0,0 +1,28 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.RCSLaunchletBehaviour = global.RCSLaunchletBehaviour || {})));
+}(this, (function (exports) { 'use strict';
+
+	let moi = {};
+
+	//# SETUP
+
+	//_ setupEverything
+
+	moi.setupEverything = function () {
+	};
+
+	//# LIFECYCLE
+
+	//_ lifecyclePageWillLoad
+
+	moi.lifecyclePageWillLoad = function () {
+		moi.setupEverything();
+	};
+
+	Object.assign(exports, moi);
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
