cc1dcd0f18b3042058c02bef5e87ba52222509e7
Rosano Coutinho <pub@xeari.com>
2019-01-27 16:30:04 -0500

    Fix typo

--- os-app/open-compose/controller.js
+++ os-app/open-compose/controller.js
@@ -3,7 +3,7 @@
 exports.OLSKControllerRoutes = function() {
 	return {
 		RCSLaunchletRoute: {
-			OLSKRoutePath: '/launchlet',
+			OLSKRoutePath: '/',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction: function(req, res, next) {
 				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {});
