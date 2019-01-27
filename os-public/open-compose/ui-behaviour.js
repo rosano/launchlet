ad926b6109b8d48ccb50b761ba4056bcc2ea0be5
Rosano Coutinho <pub@xeari.com>
2019-01-27 16:50:19 -0500

    Join data to list

--- os-public/open-compose/ui-behaviour.js
+++ os-public/open-compose/ui-behaviour.js
@@ -6,21 +6,62 @@
 
 	let moi = {};
 
-	let LCHComposeBehaviourMemberObjects = [
-		{
-			id: 'XYZGreet',
-			fn: function XYZGreet () {
-				window.prompt('Hello');
-			},
-			name: 'Greet',
-		},
-	];
+	//# PROPERTIES
+
+	//_ propertiesMemberObjects
+
+	let LCHComposeBehaviourMemberObjects;
+
+	moi.propertiesMemberObjects = function (inputData) {
+		if (typeof inputData === 'undefined') {
+			return LCHComposeBehaviourMemberObjects;
+		}
+
+		LCHComposeBehaviourMemberObjects = inputData;
+
+		moi.reactMemberObjects(LCHComposeBehaviourMemberObjects);
+	};
+
+	//# REACT
+
+	//_ reactMemberObjects
+
+	moi.reactMemberObjects = function (memberObjects) {
+		let selection = d3.select('#LCHComposeList').selectAll('.LCHComposeListItem').data(memberObjects);
+		
+		let parentElement = selection.enter().append('div')
+			.attr('class', 'LCHComposeListItem');
+
+		parentElement = parentElement.merge(selection);
+
+		parentElement
+			.text(function(obj) {
+				return obj.name;
+			});
+
+		selection.exit().remove();
+	};
 
 	//# SETUP
 
 	//_ setupEverything
 
 	moi.setupEverything = function () {
+		moi.setupListItems();
+	};
+
+	//_ setupListItems
+
+	moi.setupListItems = function () {
+		moi.propertiesMemberObjects([
+			{
+				id: 'XYZGreet',
+				fn: function XYZGreet () {
+					window.prompt('Hello');
+				},
+				name: 'Greet',
+			},
+		]);
 	};
 
 	//# LIFECYCLE
