1d6a8cc60e61c7b4c473809052cc3364d1562088
Rosano Coutinho <pub@xeari.com>
2019-07-13 12:57:42 -0400

    Save with throttle

--- os-app/open-compose-new/ModuleDetail.svelte
+++ os-app/open-compose-new/ModuleDetail.svelte
@@ -1,18 +1,40 @@
 <script>
 import * as LCHMembersAction from '../_shared/rs-modules/lch_members/action.js';
+import * as OLSKThrottle from '../_shared/_external/OLSKThrottle/main.js';
 
 import { storageClient, membersAll, memberSelected } from './persistance.js';
 
+let throttleMap = {};
+
 let _memberSelected;
 memberSelected.subscribe(function (val) {
+	if (document.querySelector('input')) {
+		document.querySelector('input').focus()
+	}
+
 	_memberSelected = val;
 });
+
+async function memberSave() {
+	if (!throttleMap[_memberSelected.LCHMemberID]) {
+		throttleMap[_memberSelected.LCHMemberID] = {
+			OLSKThrottleDuration: 500,
+			OLSKThrottleCallback: async function () {
+				delete throttleMap[_memberSelected.LCHMemberID];
+
+				await LCHMembersAction.LCHMembersActionUpdate(storageClient, _memberSelected)
+			},
+		};	
+	}
+
+	OLSKThrottle.default.OLSKThrottleTimeoutFor(throttleMap[_memberSelected.LCHMemberID]);
+}
 
 async function memberDelete() {
 	if (!window.confirm(OLSKLocalized('LCHComposeListItemDeletePromptText'))) {
 		return;
 	}
-	
+
 	membersAll.update(function (val) {
 		return val.filter(function(e) {
 			return e !== _memberSelected;
@@ -32,7 +54,7 @@ async function memberDelete() {
 		<button on:click={ memberDelete }>{ window.OLSKLocalized('LCHComposeListItemToolbarDeleteButtonText') }</button>
 	</header>
 	<div>
-		{ (_memberSelected || {}).LCHMemberID }
+		<input type="text" bind:value={ _memberSelected.LCHMemberName } on:input={ memberSave } autofocus />
 	</div>
 {/if}
 
