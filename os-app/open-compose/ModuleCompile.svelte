a1ba778b60180784967328e4ad11834043040132
Rosano Coutinho <pub@xeari.com>
2019-07-19 14:51:20 -0400

    Integrate new APIs

--- os-app/open-compose/ModuleCompile.svelte
+++ os-app/open-compose/ModuleCompile.svelte
@@ -2,27 +2,21 @@
 import { membersAll, modelDidChange } from './persistence.js';
 import * as LCHMembersModel from '../_shared/rs-modules/lch_members/model.js';
 import LCHCompile from '../open-pendext/main.js';
-import d3 from '../_shared/_external/d3-selection/dist/d3-selection.min.js';
+
+let javascriptComposed, javascriptBinary = '';
 
 import { onMount } from 'svelte';
 onMount(function () {
 	modelDidChange.subscribe(function (val) {
-		let bookmarklet = LCHCompile.LCHBookmarkletTextForTokenHash(LCHCompile.LCHTokenHashFor({
+		javascriptComposed = LCHCompile.LCHBookmarkletTextForTokenHashNew(LCHCompile.LCHTokenHashForNew({
 			LCHInputMemberObjects: $membersAll.filter(function (e) {
 				return !!e.LCHMemberBody;
 			}).map(LCHMembersModel.LCHMembersModelConvertLegacy).map(LCHCompile.LCHWrappedMemberObjectFor),
-			LCHInputStyleContent: window.LCHComposeStyleContent,
-			LCHInputLibraryD3Content: window.LCHComposeLibraryD3Content,
+			LCHInputAppBehaviour: window.LCHComposeBookmarkletBehaviour.replace(`//# sourceMappingURL=ui-behaviour.js.map`, ''),
+			LCHInputAppStyle: window.LCHComposeBookmarkletStyle,
 		}));
 
-		d3.select('#LCHComposeComposedSample')
-			.property('value', bookmarklet);
-
-		d3.select('#LCHComposeBinary')
-			.property('value', LCHCompile.LCHBookmarkletBinaryFor(bookmarklet));
-
-		d3.select('#LCHComposeBuildLink')
-			.property('href', d3.select('#LCHComposeBinary').property('value'));
+		javascriptBinary = LCHCompile.LCHBookmarkletBinaryFor(javascriptComposed);
 	});
 });
 </script>
@@ -30,15 +24,11 @@ onMount(function () {
 <div class="Container">
 
 <p>
-	<textarea id="LCHComposeComposedSample" onclick="this.select()"></textarea>
+	<textarea onclick="this.select()">{ javascriptComposed }</textarea>
 </p>
 
 <p>
-	<textarea id="LCHComposeBinary" onclick="this.select()"></textarea>
-</p>
-
-<p>
-	<a id="LCHComposeBuildLink" href="#"  accesskey="r">Try it</a>
+	<a id="LCHComposeBuildLink" href="{ javascriptBinary }"  accesskey="r">Try it</a>
 </p>
 
 </div>
