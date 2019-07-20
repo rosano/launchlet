f32cf1a5f52799280dcbd8113a34685bc377a2e8
Rosano Coutinho <pub@xeari.com>
2019-07-20 09:00:02 -0400

    Install mocha-start

--- mocha-start.js
+++ mocha-start.js
@@ -1,40 +1,7 @@
 //# OLSKMochaReplaceES6Import
 
 (function OLSKMochaReplaceES6Import() {
-	const fs = require('fs');
-
-	require.extensions['.js'] = function(module, filename) {
-		const exportable = [];
-
-		let code = fs.readFileSync(filename, 'utf-8')
-			.replace(/^import \* as (\w+) from ['"]([^'"]+)['"];?/gm, 'var $1 = require("$2");')
-			.replace(/^import (\w+) from ['"]([^'"]+)['"];?/gm, 'var {default: $1} = require("$2");')
-			.replace(/^import {([^}]+)} from ['"](.+)['"];?/gm, 'var {$1} = require("$2");')
-			.replace(/^export default /gm, 'exports.default = ')
-			.replace(/^export (const|let|var|class|function) (\w+)/gm, (match, type, name) => {
-				exportable.push(name);
-				return `${type} ${name}`;
-			})
-			.replace(/^export \{([^}]+)\}(?: from ['"]([^'"]+)['"];?)?/gm, (match, names, source) => {
-				names.split(',').filter(Boolean).forEach(name => {
-					exportable.push(name);
-				});
-
-				return source ? `const { ${names} } = require("${source}");` : '';
-			})
-			.replace(/^export function (\w+)/gm, 'exports.$1 = function $1');
-
-		exportable.forEach(name => {
-			code += `\nexports.${name} = ${name};`;
-		});
-
-		try {
-			return module._compile(code, filename);
-		} catch (err) {
-			console.log(code); // eslint-disable-line no-console
-			throw err;
-		}
-	};
+	require('OLSKTesting')._OLSKTestingMochaReplaceES6Import();
 })();
 
 //# LCHMochaSetup
