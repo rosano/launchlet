c3dbfcbc6e897367122ba162f19aab66048ef4a3
Rosano Coutinho <pub@xeari.com>
2019-07-21 17:50:46 -0400

    Rename PLUGIN_ALFA_SEARCH_REPLACE

--- os-app/dev-bookmarklet/_shared.js
+++ os-app/dev-bookmarklet/_shared.js
@@ -5,7 +5,7 @@ export const languageCode = function(inputData) {
 	return !inputData ? _languageCode : (_languageCode = inputData);
 };
 
-let localizationDictionary = JSON.parse(`{"PLUGIN_ALFA_SEARCH_REPLACE":"PLUGIN_ALFA_SEARCH_REPLACE"}`);
+let localizationDictionary = JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`);
 export const OLSKLocalized = function(translationConstant) {
 	return OLSKInternational.OLSKInternationalLocalizedStringWithTranslationKeyAndTranslationDictionary(translationConstant, localizationDictionary[languageCode()]);
 };
