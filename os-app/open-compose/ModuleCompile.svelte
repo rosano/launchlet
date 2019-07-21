b21ac6ddba3190ae36176e4e5e64667522d823e4
Rosano Coutinho <pub@xeari.com>
2019-07-21 12:28:00 -0400

    Use LCHCompile.LCHBookmarkletStringFor

--- os-app/open-compose/ModuleCompile.svelte
+++ os-app/open-compose/ModuleCompile.svelte
@@ -8,13 +8,14 @@ let javascriptComposed, javascriptBinary = '';
 import { onMount } from 'svelte';
 onMount(function () {
 	modelDidChange.subscribe(function (val) {
-		javascriptComposed = LCHCompile.LCHBookmarkletTextForTokenHashNew(LCHCompile.LCHTokenHashForNew({
-			LCHInputMemberObjects: $membersAll.filter(function (e) {
+		javascriptComposed = LCHCompile.LCHBookmarkletStringFor({
+			LCHToken_AppStyle: window.LCHComposeBookmarkletStyle,
+			LCHToken_AppBehaviour: window.LCHComposeBookmarkletBehaviour.replace(`//# sourceMappingURL=ui-behaviour.js.map`, ''),
+			LCHToken_MemberObjects: $membersAll.filter(function (e) {
 				return !!e.LCHMemberBody;
 			}).map(LCHMembersModel.LCHMembersModelConvertLegacy).map(LCHCompile.LCHWrappedMemberObjectFor),
-			LCHInputAppBehaviour: window.LCHComposeBookmarkletBehaviour.replace(`//# sourceMappingURL=ui-behaviour.js.map`, ''),
-			LCHInputAppStyle: window.LCHComposeBookmarkletStyle,
-		}));
+			LCHToken_AppLanguageCode: window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage'),
+		});
 
 		javascriptBinary = LCHCompile.LCHBookmarkletBinaryFor(javascriptComposed);
 	});
