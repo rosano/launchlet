e594083667455647a51454b297f577aff0610db7
Rosano Coutinho <pub@xeari.com>
2019-07-26 11:13:52 -0400

    Prepend normalize to css

--- os-app/dev-bookmarklet/rollup-config-custom.js
+++ os-app/dev-bookmarklet/rollup-config-custom.js
@@ -1,3 +1,9 @@
+const svelte = require('rollup-plugin-svelte');
+
+const pathPackage = require('path');
+
+const production = !process.env.ROLLUP_WATCH;
+
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 		typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -31,6 +37,30 @@
 	},
 	
 	OLSKRollupConfigCustomFor (inputData) {
+		inputData.plugins.splice(0, 1, svelte({
+
+			preprocess: {
+				style({ content, filename }) {
+					return {
+						code: mod.LCHRollupPrefixSelector(mod.LCHRollupGrabContainerSelector(content), require('fs').readFileSync(pathPackage.join(__dirname, '../_shared/_external/normalize.css/normalize.css'), 'utf8')) + content,
+						map: ''
+					};
+				},
+			},
+
+			// --- COPY PREVIOUS CONFIGURATION ---
+
+			// enable run-time checks when not in production
+			dev: !production,
+
+			// extract component CSS into separate file for better performance
+			css: function (css) {
+				// console.log(mod.LCHRollupPrefixSelector(mod.LCHRollupGrabContainerSelector(css.code), require('fs').readFileSync(pathPackage.join(__dirname, '../_shared/_external/normalize.css/normalize.css'), 'utf8')));
+				// css.code = 'hello { alfa: bravo; } ' + css.code
+				return css.write(pathPackage.join(pathPackage.dirname(inputData.input), '_compiled/ui-style.css'));
+			},
+		}));
+
 		return inputData;
 	},
 	
