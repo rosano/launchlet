6794ae8f557860a8827d8e62f27520c0f29e2f88
Rosano Coutinho <pub@xeari.com>
2019-08-01 17:13:20 -0400

    Spec LCHCopyToClipboardUITestDiscovery

---
+++ os-app/dev-launcher/formulas/LCHCopyToClipboard/ui-behaviour-tests.js
@@ -0,0 +1,40 @@
+import { throws, deepEqual } from 'assert';
+
+const Browser = require('zombie');
+
+Browser.localhost('loc.tests', 3000);
+
+Object.entries({
+	LCHLauncherFilterInput: '#LCHLauncherFilterInput',
+
+	LCHLauncherListItem: '.ListItem',
+
+	LCHCopyToClipboardButton: '#LCHCopyToClipboardButton',
+}).map(function (e) {
+	return global[e.shift()]  = e.pop();
+});
+
+const browser = new Browser();
+
+describe('LCHCopyToClipboardUITestDiscovery', function testDiscovery() {
+
+	before(function() {
+		return browser.visit('/launcher?loadModules=LCHCopyToClipboard');
+	});
+	
+	it('on startup', function() {
+		browser.assert.elements(LCHCopyToClipboardButton, 0);
+	});
+
+	it('on run', async function() {
+		browser.fill(LCHLauncherFilterInput, 'LCHCopyToClipboardTest');
+		await browser.wait({element: LCHLauncherListItem});
+
+		browser.click(LCHLauncherListItem);
+		await browser.wait({element: LCHCopyToClipboardButton});
+
+		browser.assert.elements(LCHCopyToClipboardButton, 1);
+	});
+
+});
+
