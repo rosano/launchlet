3a72f86dc22b576da3f5c2016f37ce83781b9eb3
Rosano Coutinho <pub@xeari.com>
2019-08-01 12:55:31 -0400

    Add navigator api to copy to clipboard

--- os-app/dev-launcher/formulas/LCHCopyToClipboard/main.js
+++ os-app/dev-launcher/formulas/LCHCopyToClipboard/main.js
@@ -7,17 +7,27 @@ export const LCHCopyToClipboard = function(inputData) {
 		throw new Error('LCHErrorInputInvalid');
 	}
 
-	if (typeof document === 'undefined') {
-		return;
+	if (typeof navigator !== 'undefined' && navigator.clipboard) {
+		return (async function () {
+			return await navigator.clipboard.writeText(inputData);
+		})();
 	}
 
-	const el = document.createElement('textarea');
-	el.value = inputData;
-	el.setAttribute('readonly', '');
-	el.style.position = 'fixed';
-	el.style.top = 0;
-	document.body.appendChild(el);
-	el.select();
-	document.execCommand('copy');
-	el.remove();
+	if (typeof document !== 'undefined') {
+		(function () {
+			const el = document.createElement('textarea');
+			
+			el.value = inputData;
+			
+			el.setAttribute('readonly', '');
+			el.style.position = 'fixed';
+			el.style.top = 0;
+			
+			document.body.appendChild(el);
+			el.select();
+			document.execCommand('copy');
+			
+			el.remove();
+		})();
+	}
 };
