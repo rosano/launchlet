e359675cc3de57c5036b33865e7dec14155c05d1
Rosano Coutinho <pub@xeari.com>
2019-08-01 16:38:24 -0400

    Define LCHCopyToClipboard component

---
+++ os-app/dev-launcher/formulas/LCHCopyToClipboard/Main.svelte
@@ -0,0 +1,47 @@
+<script>
+export let inputData = '';
+export let completionHandler = null;
+
+import { onMount } from 'svelte';
+import Clipboard from 'clipboard';
+let clipboardButton, clipboard;
+
+let _didComplete = false;
+function didComplete() {
+	if (_didComplete) {
+		return;
+	}
+
+	clipboard.destroy();
+
+	completionHandler();
+
+	_didComplete = true;
+};
+
+onMount(function () {
+	clipboard = new Clipboard(clipboardButton);
+
+	clipboard.on('success', function(e) {
+		// console.log('success', e);
+	  didComplete();
+	});
+
+	clipboard.on('error', function(e) {
+		// console.log('error', e);
+	  didComplete();
+	});
+
+	clipboardButton.addEventListener('click', function (e) {
+		// console.log('click', e);
+		didComplete();
+	})
+
+	clipboardButton.focus();
+});
+</script>
+
+<button data-clipboard-text={ inputData } bind:this={ clipboardButton }>Copy to clipboard</button>
+
+<style>
+</style>
