9ded54ca4be41bcdc8464f880b9e8faf9402ccf0
Rosano Coutinho <pub@xeari.com>
2019-08-01 17:11:41 -0400

    Include ui-data when specified in loadModules

--- os-app/dev-launcher/controller.js
+++ os-app/dev-launcher/controller.js
@@ -6,7 +6,9 @@ exports.OLSKControllerRoutes = function() {
 			OLSKRoutePath: '/launcher',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction: function(req, res, next) {
-				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {});
+				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {
+					LCHLauncherQuery: req.query,
+				});
 			},
 			OLSKRouteLanguages: ['en'],
 		},
