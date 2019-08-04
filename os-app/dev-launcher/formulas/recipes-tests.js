e40de190f57f0dee8f936f69498d361837e09407
Rosano Coutinho <pub@xeari.com>
2019-08-04 08:23:13 -0400

    Sort recipes in test

--- os-app/dev-launcher/formulas/recipes-tests.js
+++ os-app/dev-launcher/formulas/recipes-tests.js
@@ -5,7 +5,16 @@ import * as mainModule from './recipes.js';
 describe('LCHLauncherStandardRecipes', function testLCHLauncherStandardRecipes() {
 
 	it('returns LCHFormulaObject for each folder', function() {
-		deepEqual(mainModule.LCHLauncherStandardRecipes(), [].concat.apply([], require('glob').sync('*main.js', {
+		deepEqual(mainModule.LCHLauncherStandardRecipes().sort(function (a, b) {
+			if ( a.LCHRecipeSignature < b.LCHRecipeSignature) {
+				return -1;
+			}
+			if ( a.LCHRecipeSignature > b.LCHRecipeSignature) {
+				return 1;
+			}
+			
+			return 0;
+		}), [].concat.apply([], require('glob').sync('*main.js', {
 		  matchBase: true,
 		  cwd: __dirname,
 		}).map(function (e) {
