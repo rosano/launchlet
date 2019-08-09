a775d31170265ba4bb684c344e4e40dc1993011e
Rosano Coutinho <pub@xeari.com>
2019-08-09 12:41:01 -0400

    Create svelte stub view

--- os-app/dev-launcher/modules/LCHLauncherPrompt/rollup-start.js
+++ os-app/dev-launcher/modules/LCHLauncherPrompt/rollup-start.js
@@ -1,4 +1,4 @@
-import App from './main.svelte';
+import App from './stub-view.svelte';
 
 const LCHLauncherPrompt = new App({
 	target: document.body,
---
+++ os-app/dev-launcher/modules/LCHLauncherPrompt/stub-view.svelte
@@ -0,0 +1,18 @@
+<script>
+import Module from './main.svelte';
+export let StubItems = [];
+let StubItemSelected = null;
+
+export let HeaderText = undefined;
+export let FilterText = undefined;
+export let MatchStop = false;
+</script>
+
+<Module PromptItems={ StubItems } on:ItemSelectedDidChange={ (event) => StubItemSelected = event.detail } HeaderText={ HeaderText } FilterText={ FilterText } MatchStop={ MatchStop } />
+
+<hr>
+
+<p>
+	<strong>StubItemSelected</strong>
+	<span id="LCHLauncherPromptStubItemSelected">{ (StubItemSelected || {}).LCHRecipeTitle }</span>
+</p>
