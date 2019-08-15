12765f524e24f3f37fa6698225a153d3c9a8325c
Rosano Coutinho <pub@xeari.com>
2019-08-15 13:10:56 -0400

    Spec LCHSearchWithURLFrom

--- os-app/dev-launcher/recipes/LCHSearchWith/main-tests.js
+++ os-app/dev-launcher/recipes/LCHSearchWith/main-tests.js
@@ -2,6 +2,42 @@ import { throws, deepEqual } from 'assert';
 
 import * as mainModule from './main.js';
 
+describe('LCHSearchWithURLFrom', function testLCHSearchWithURLFrom() {
+
+	it('throws error if param1 not string', function() {
+		throws(function() {
+			mainModule.LCHSearchWithURLFrom(null, '');
+		}, /LCHErrorInputInvalid/);
+	});
+
+	it('throws error if param2 not string', function() {
+		throws(function() {
+			mainModule.LCHSearchWithURLFrom('', null);
+		}, /LCHErrorInputInvalid/);
+	});
+
+	it('returns param1', function() {
+		deepEqual(mainModule.LCHSearchWithURLFrom('alfa', ''), 'alfa');
+	});
+
+	describe('search token', function() {
+
+		it('replaces with param2', function() {
+			deepEqual(mainModule.LCHSearchWithURLFrom('alfa?LCH+SEARCH+TOKEN', 'bravo'), 'alfa?bravo');
+		});
+
+		it('substitutes delimiters', function() {
+			deepEqual(mainModule.LCHSearchWithURLFrom('LCH+SEARCH+TOKEN', 'bravo charlie'), 'bravo+charlie');
+		});
+
+		it('matches %20', function() {
+			deepEqual(mainModule.LCHSearchWithURLFrom('LCH%20SEARCH%20TOKEN', 'bravo charlie'), 'bravo%20charlie');
+		});
+
+	});
+
+});
+
 describe('LCHSearchWithCallback', function testLCHSearchWithCallback() {
 
 	it('calls api with result', async function() {
--- os-app/dev-launcher/recipes/LCHSearchWith/main.js
+++ os-app/dev-launcher/recipes/LCHSearchWith/main.js
@@ -1,3 +1,21 @@
+export const LCHSearchWithURLFrom = function(param1, param2) {
+	if (typeof param1 !== 'string') {
+		throw new Error('LCHErrorInputInvalid');
+	}
+
+	if (typeof param2 !== 'string') {
+		throw new Error('LCHErrorInputInvalid');
+	}
+
+	let match = param1.match(/LCH(.*)SEARCH(.*)TOKEN/i);
+
+	if (!match || !match[1]) {
+		return param1;
+	};
+
+	return param1.replace(match[0], param2.split(' ').join(match[1]));
+};
+
 export const LCHSearchWithCallback = function(param1, param2) {
 	return this.api.fn('ROCOLaunchURL')(param2.LCHRecipeCallback(param1));
 };
