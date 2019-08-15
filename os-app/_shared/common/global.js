8c7160a1d95ee5568fc0b5bde4b3bf81d9a487ab
Rosano Coutinho <pub@xeari.com>
2019-08-15 11:57:13 -0400

    Cleanup

--- os-app/_shared/common/global.js
+++ os-app/_shared/common/global.js
@@ -1,6 +1,5 @@
-import OLSKInternational from 'OLSKInternational';
-
 let localizationDictionary = JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`);
+import OLSKInternational from 'OLSKInternational';
 export const OLSKLocalized = function(translationConstant) {
 	return OLSKInternational.OLSKInternationalLocalizedStringWithTranslationKeyAndTranslationDictionary(translationConstant, localizationDictionary[window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage')]);
 };
