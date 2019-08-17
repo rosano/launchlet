f4683abe9448a805b169911f380788d7d1e8c44b
Rosano Coutinho <pub@xeari.com>
2019-08-17 12:23:41 -0400

    Sort both sides

--- os-app/dev-launcher/recipes/recipes-tests.js
+++ os-app/dev-launcher/recipes/recipes-tests.js
@@ -2,27 +2,31 @@ import { throws, deepEqual } from 'assert';
 
 import * as mainModule from './recipes.js';
 
+const uSort = function (inputData) {
+	return inputData.map(function (e) {
+		return Object.assign(e, {
+			_TestSortKey: e.LCHRecipeSignature.indexOf('LCH') === -1 ? `Type${ e.LCHRecipeSignature }` : e.LCHRecipeSignature,
+		});
+	}).sort(function (a, b) {
+		if ( a._TestSortKey < b._TestSortKey) {
+			return -1;
+		}
+		if ( a._TestSortKey > b._TestSortKey) {
+			return 1;
+		}
+		
+		return 0;
+	}).map(function (e) {
+		delete e._TestSortKey;
+		
+		return e;
+	});
+}
+
 describe('LCHLauncherStandardRecipes', function testLCHLauncherStandardRecipes() {
 
 	it('returns LCHFormulaObject for each folder', function() {
-		deepEqual(mainModule.LCHLauncherStandardRecipes().map(function (e) {
-			return Object.assign(e, {
-				_TestSortKey: e.LCHRecipeSignature.indexOf('LCH') === -1 ? `Type${ e.LCHRecipeSignature }` : e.LCHRecipeSignature,
-			});
-		}).sort(function (a, b) {
-			if ( a._TestSortKey < b._TestSortKey) {
-				return -1;
-			}
-			if ( a._TestSortKey > b._TestSortKey) {
-				return 1;
-			}
-			
-			return 0;
-		}).map(function (e) {
-			delete e._TestSortKey;
-			
-			return e;
-		}), [].concat.apply([], require('glob').sync('*main.js', {
+		deepEqual(uSort(mainModule.LCHLauncherStandardRecipes()), uSort([].concat.apply([], require('glob').sync('*main.js', {
 		  matchBase: true,
 		  cwd: __dirname,
 		}).map(function (e) {
@@ -31,7 +35,7 @@ describe('LCHLauncherStandardRecipes', function testLCHLauncherStandardRecipes()
 			}).map(function (e) {
 				return e.pop()();
 			});
-		})));
+		}))));
 	});
 
 });
