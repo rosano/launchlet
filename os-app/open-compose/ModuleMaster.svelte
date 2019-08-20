c10f4b989917b250d5e8dc85dcf96f0394e4d695
Rosano Coutinho <pub@xeari.com>
2019-08-20 13:18:00 -0400

    Set default value of LCHDocumentOutputTypeCanonicalExampleBody

--- os-app/open-compose/ModuleMaster.svelte
+++ os-app/open-compose/ModuleMaster.svelte
@@ -88,6 +88,7 @@ const mod = {
 			LCHDocumentArgs: '',
 			LCHDocumentBody: '',
 			LCHDocumentOutputType: '',
+			LCHDocumentOutputTypeCanonicalExampleBody: '',
 			LCHDocumentSignature: '',
 			LCHDocumentURLFilter: '',
 			LCHDocumentStyle: '',
