fbd842810883b6d3d813f2938def4dd316e4debe
Rosano Coutinho <pub@xeari.com>
2019-08-26 10:30:30 -0400

    Create package-preinstall

---
+++ package-preinstall.js
@@ -0,0 +1,31 @@
+//# OLSKPreinstallPurgeOLSK
+
+(function OLSKPreinstallPurgeOLSK() {
+	if (process.env.NODE_ENV !== 'production') {
+		return;
+	}
+
+	try {
+		require('glob');
+	} catch(e) {
+		return console.log(e);
+	}
+
+	(function (param1, param2) {
+		return require('glob').sync(`+(${ param1.join('|') })/`, {
+			matchBase: true,
+			cwd: param2,
+		}).forEach(function (e) {
+			try {
+				require('OLSKDisk').OLSKDiskDeleteFolder(require('path').join(param2, e));
+			} catch (e) {
+				console.warn('error deleting folder');
+			}
+		});
+	})([
+		'OLSK*',
+		'launchlet',
+		'rollup-plugin-i18n',
+	], require('path').join(__dirname, 'node_modules'));
+
+})();
