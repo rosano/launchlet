fd79a3bb3a1df68f07e47f4f3d8a86e83bc94210
Rosano Coutinho <pub@xeari.com>
2019-08-26 17:50:25 -0400

    Renname compose route constant

--- os-app/open-compose/controller.js
+++ os-app/open-compose/controller.js
@@ -2,7 +2,7 @@
 
 exports.OLSKControllerRoutes = function() {
 	return {
-		LCHComposeNewRoute: {
+		LCHComposeRoute: {
 			OLSKRoutePath: '/',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction: function (req, res, next) {
