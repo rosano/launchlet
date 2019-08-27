5c263d33242e1b763a221f04dfd1ded82addb4f0
Rosano Coutinho <pub@xeari.com>
2019-08-27 17:39:33 -0400

    Fix typo

--- os-app/dev-launcher/rollup-config-custom.js
+++ os-app/dev-launcher/rollup-config-custom.js
@@ -37,7 +37,6 @@ module.exports = {
 				autoPreprocess({}),
 				{
 					style({ content, filename }) {
-						console.log(content, filename);
 						return {
 							code: (filename.match(pathPackage.join(__dirname, 'Main.svelte')) ? module.exports.LCHRollupPrefixSelector(module.exports.LCHRollupGrabContainerSelector(content), require('fs').readFileSync(pathPackage.join(__dirname, '../_shared/__external/normalize.css/normalize.css'), 'utf8')) : '') + content,
 							map: ''
