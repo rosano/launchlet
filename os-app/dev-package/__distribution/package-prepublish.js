9a0988bb5dc54967f505f5dc1930d4184c8a0aca
Rosano Coutinho <pub@xeari.com>
2019-08-26 20:06:26 -0400

    Restore package files

---
+++ os-app/dev-package/__distribution/package-prepublish.js
@@ -0,0 +1,15 @@
+//# OLSKPrepublishCopyPackage
+
+(function OLSKPrepublishCopyPackage() {
+	const globPackage = require('glob');
+	const fsPackage = require('fs');
+	const pathPackage = require('path');
+
+	let sourceDir = pathPackage.join(__dirname, '../os-app/dev-package/_compiled/dist');
+	return globPackage.sync('launchlet*', {
+		matchBase: true,
+		cwd: sourceDir,
+	}).forEach(function(e) {
+		fsPackage.copyFileSync(pathPackage.join(sourceDir, e), pathPackage.join(__dirname, e));
+	});
+})();
