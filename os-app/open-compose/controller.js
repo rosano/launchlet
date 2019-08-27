02c1bb7e55fd53e261fdaf11acc0b15dde6faea8
Rosano Coutinho <pub@xeari.com>
2019-08-27 16:11:02 -0400

    Lint

--- os-app/open-compose/controller.js
+++ os-app/open-compose/controller.js
@@ -9,7 +9,7 @@ exports.OLSKControllerRoutes = function() {
 				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {
 					LCHComposeLauncherStyle: require('fs').readFileSync(req.OLSKLive.OLSKLivePathJoin(__dirname, '../dev-launcher/__compiled/ui-style.css'), 'utf8'),
 					LCHComposeLauncherBehaviour: require('fs').readFileSync(req.OLSKLive.OLSKLivePathJoin(__dirname, '../dev-launcher/__compiled/ui-behaviour.js'), 'utf8'),
-					LCHDropboxAppKey: Buffer.from(process.env.LCH_DROPBOX_APP_KEY).toString('base64'),					
+					LCHDropboxAppKey: Buffer.from(process.env.LCH_DROPBOX_APP_KEY).toString('base64'),
 				});
 			},
 			OLSKRouteLanguages: ['en'],
