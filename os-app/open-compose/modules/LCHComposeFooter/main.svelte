8326d8728005f762bbd5120da5733765fa90c4aa
Rosano Coutinho <pub@xeari.com>
2019-08-27 15:25:19 -0400

    Enable export/import

--- os-app/open-compose/modules/LCHComposeFooter/main.svelte
+++ os-app/open-compose/modules/LCHComposeFooter/main.svelte
@@ -33,9 +33,9 @@ const mod = {
 		<OLSKToolbarElementGroup>
 			<OLSKReloadButton OLSKLocalized={ OLSKLocalized } />
 
-			<!-- <button on:click={ mod.interfaceExportButtonDidClick }>Export</button> -->
+			<button on:click={ mod.interfaceExportButtonDidClick }>Export</button>
 
-			<!-- <input type="file" accept=".json" on:change={ mod.interfaceImportButtonDidClick } /> -->
+			<input type="file" accept=".json" on:change={ mod.interfaceImportButtonDidClick } />
 		</OLSKToolbarElementGroup>
 		<RCSLanguageSwitcher />
 	</OLSKToolbar>
