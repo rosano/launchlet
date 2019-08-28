41eed440f8189ab6d31a9d50fbc3ed5a33703597
Rosano Coutinho <pub@xeari.com>
2019-08-28 17:22:41 -0400

    Purge recipe sorting

--- os-app/dev-launcher/recipes/main-tests.js
+++ os-app/dev-launcher/recipes/main-tests.js
@@ -2,31 +2,10 @@ import { throws, deepEqual } from 'assert';
 
 import * as mainModule from './main.js';
 
-const uSort = function (inputData) {
-	return inputData.map(function (e) {
-		return Object.assign(e, {
-			_TestSortKey: e.LCHRecipeSignature.indexOf('LCH') === -1 ? `Type${ e.LCHRecipeSignature }` : e.LCHRecipeSignature,
-		});
-	}).sort(function (a, b) {
-		if ( a._TestSortKey < b._TestSortKey) {
-			return -1;
-		}
-		if ( a._TestSortKey > b._TestSortKey) {
-			return 1;
-		}
-		
-		return 0;
-	}).map(function (e) {
-		delete e._TestSortKey;
-		
-		return e;
-	});
-};
-
 describe('LCHLauncherStandardRecipes', function testLCHLauncherStandardRecipes() {
 
 	it('returns LCHFormulaObject for each folder', function() {
-		deepEqual(uSort(mainModule.LCHLauncherStandardRecipes()), uSort([].concat.apply([], require('glob').sync('*/**/main.js', {
+		const items = [].concat.apply([], require('glob').sync('*/**/main.js', {
 		  matchBase: true,
 		  cwd: __dirname,
 		}).map(function (e) {
@@ -35,7 +14,13 @@ describe('LCHLauncherStandardRecipes', function testLCHLauncherStandardRecipes()
 			}).map(function (e) {
 				return e.pop()();
 			});
-		}))));
+		}));
+
+		deepEqual(mainModule.LCHLauncherStandardRecipes().filter(function (e) {
+			return !items.filter(function (e2) {
+				return e2.LCHRecipeCallback === e.LCHRecipeCallback;
+			}).length;
+		}), []);
 	});
 
 });
