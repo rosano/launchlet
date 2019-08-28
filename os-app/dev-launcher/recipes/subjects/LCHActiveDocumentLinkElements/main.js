893bcd62d1082c7c7fc420d7fc4811fc9071ee37
Rosano Coutinho <pub@xeari.com>
2019-08-28 11:49:46 -0400

    Update document instance

--- os-app/dev-launcher/recipes/subjects/LCHActiveDocumentLinkElements/main.js
+++ os-app/dev-launcher/recipes/subjects/LCHActiveDocumentLinkElements/main.js
@@ -25,7 +25,7 @@ export const LCHActiveDocumentLinkElementsCallback = function() {
 export const LCHActiveDocumentLinkElementsRecipe = function() {
 	return {
 		LCHRecipeName: 'Active Document Link Elements',
-		LCHRecipeOutputType: 'Container',
+		LCHRecipeOutputType: '[Subject]',
 		LCHRecipeCallback: LCHActiveDocumentLinkElementsCallback,
 		LCHRecipeSignature: 'LCHActiveDocumentLinkElements',
 	};
