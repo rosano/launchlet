61878d33262f3fd74a59c987a5f5020795b54c43
Rosano Coutinho <pub@xeari.com>
2019-08-28 11:33:13 -0400

    Spec LCHLinkElements

--- os-app/dev-launcher/recipes/subjects/LCHActiveDocumentLinkElements/main-tests.js
+++ os-app/dev-launcher/recipes/subjects/LCHActiveDocumentLinkElements/main-tests.js
@@ -1,6 +1,55 @@
 import { throws, deepEqual } from 'assert';
+import { JSDOM } from 'jsdom';
 
 import * as mainModule from './main.js';
+
+describe('LCHLinkElements', function testLCHLinkElements() {
+
+	it('throws if not object', function() {
+		throws(function() {
+			mainModule.LCHLinkElements(null);
+		}, /LCHErrorInputInvalid/);
+	});
+
+	it('throws if not DOMDocument', function() {
+		throws(function() {
+			mainModule.LCHLinkElements({});
+		}, /LCHErrorInputInvalid/);
+	});
+
+	it('returns array', function() {
+		deepEqual(mainModule.LCHLinkElements(JSDOM.fragment('')), []);
+	});
+
+	it('excludes if no href', function() {
+		deepEqual(mainModule.LCHLinkElements(JSDOM.fragment('<a href="">alfa</a>')), []);
+	});
+
+	it('excludes if no textContent or title', function() {
+		deepEqual(mainModule.LCHLinkElements(JSDOM.fragment('<a href="#"></a>')), []);
+	});
+
+	it('excludes if textContent only whitespace', function() {
+		deepEqual(mainModule.LCHLinkElements(JSDOM.fragment('<a href="#"> </a>')), []);
+	});
+
+	it('excludes if title only whitespace', function() {
+		deepEqual(mainModule.LCHLinkElements(JSDOM.fragment('<a href="#" title=" "></a>')), []);
+	});
+
+	it('includes if textContent', function() {
+		deepEqual(mainModule.LCHLinkElements(JSDOM.fragment('<a href="#">alfa</a>')).pop().LCHRecipeName, 'alfa');
+	});
+
+	it('includes if title', function() {
+		deepEqual(mainModule.LCHLinkElements(JSDOM.fragment('<a href="#" title="alfa"></a>')).pop().LCHRecipeName, 'alfa');
+	});
+
+	it('prefers textContent if both', function() {
+		deepEqual(mainModule.LCHLinkElements(JSDOM.fragment('<a href="#" title="bravo">alfa</a>')).pop().LCHRecipeName, 'alfa');
+	});
+
+});
 
 describe('LCHActiveDocumentLinkElementsCallback', function testLCHActiveDocumentLinkElementsCallback() {
 
--- os-app/dev-launcher/recipes/subjects/LCHActiveDocumentLinkElements/main.js
+++ os-app/dev-launcher/recipes/subjects/LCHActiveDocumentLinkElements/main.js
@@ -1,3 +1,24 @@
+export const LCHLinkElements = function(inputData) {
+	if (typeof inputData !== 'object' || inputData === null || typeof inputData.querySelectorAll !== 'function') {
+		throw new Error('LCHErrorInputInvalid');
+	}
+	return [].concat.apply([], inputData.querySelectorAll('a')).filter(function (e) {
+		if (!e.href) {
+			return false;
+		};
+		
+		if (!e.textContent.trim() && !e.title.trim()) {
+			return false;
+		};
+		
+		return true;
+	}).map(function (e) {
+		return {
+			LCHRecipeName: e.textContent.trim() || e.title.trim(),
+		};
+	});
+};
+
 export const LCHActiveDocumentLinkElementsCallback = function() {
 };
 
