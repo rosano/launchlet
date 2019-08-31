dcb1b651e5bff3062fe8b7cc2020b12322efcfc3
Rosano Coutinho <pub@xeari.com>
2019-08-31 10:23:28 -0400

    Stub vitrine content

--- os-app/open-vitrine/controller.js
+++ os-app/open-vitrine/controller.js
@@ -6,7 +6,9 @@ exports.OLSKControllerRoutes = function() {
 			OLSKRoutePath: '/',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction: function (req, res, next) {
-				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'view'), {});
+				return res.render(require('path').join(__dirname, 'view'), {
+					LCHVitrineContent: require('marked')(require('fs').readFileSync(require('path').join(__dirname, `content.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8')),
+				});
 			},
 			OLSKRouteLanguages: ['en'],
 		},
