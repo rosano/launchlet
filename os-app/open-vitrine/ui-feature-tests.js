313e83c50ebf3ee343b6def8fbc602cf0e5942b7
Rosano Coutinho <pub@xeari.com>
2019-08-31 13:26:56 -0400

    Spec shows launcher

---
+++ os-app/open-vitrine/ui-feature-tests.js
@@ -0,0 +1,23 @@
+import { deepEqual } from 'assert';
+
+const browser = new OLSKBrowser();
+const kDefaultRoutePath = require('./controller.js').OLSKControllerRoutes().LCHVitrineRoute.OLSKRoutePath;
+
+describe('LCHVitrineUIFeature', function () {
+
+context('LCHVitrineDemoButtonCommit', function testLCHVitrineDemoButtonCommit () {
+
+	before(function() {
+		return browser.visit(kDefaultRoutePath);
+	});
+	
+	it('shows launcher', async function() {
+		browser.click(LCHVitrineDemoButtonCommit);
+		await browser.wait({element: LCHLauncherFilterInput});
+
+		browser.assert.elements(LCHLauncherFilterInput, 1);
+	});
+
+});
+
+});
