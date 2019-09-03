31d94fdb84094632d04ddf9674ac8a66a7626b44
Rosano Coutinho <pub@xeari.com>
2019-09-03 19:11:35 -0400

    Spec package contains launcher style

---
+++ os-app/dev-package/ui-style-tests.js
@@ -0,0 +1,9 @@
+import { deepEqual } from 'assert';
+
+describe('LCHPackageStyle', function testLCHPackageStyle() {
+
+	it('contains launcher style', function() {
+		deepEqual(require('fs').readFileSync(require('path').join(__dirname, '__compiled/launchlet.css'), 'utf8').slice(0, 100), require('fs').readFileSync(require('path').join(__dirname, '../dev-launcher/__compiled/ui-style.css'), 'utf8').slice(0, 100));
+	});
+
+});
