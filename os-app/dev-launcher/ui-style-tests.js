2da9ddff57cd837da78af43ac5263e74f947f981
Rosano Coutinho <pub@xeari.com>
2019-09-03 19:16:19 -0400

    Spec prefixes normalize

--- os-app/dev-launcher/ui-style-tests.js
+++ os-app/dev-launcher/ui-style-tests.js
@@ -1,9 +1,15 @@
 import { deepEqual } from 'assert';
 
+const inputData = require('fs').readFileSync(require('path').join(__dirname, '__compiled/ui-style.css'), 'utf8');
+
 describe('LCHLauncherStyle', function testLCHLauncherStyle() {
 
 	it('contains normalize', function() {
-		deepEqual(require('fs').readFileSync(require('path').join(__dirname, '__compiled/ui-style.css'), 'utf8').match(/\{(.*00%)\}/)[1], 'line-height:1.15;-webkit-text-size-adjust:100%');
+		deepEqual(inputData.match(/\{(.*00%)\}/)[1], 'line-height:1.15;-webkit-text-size-adjust:100%');
+	});
+
+	it('prefixes normalize', function() {
+		deepEqual(typeof inputData.match(/\.Container\.svelte-\w+\{line-height:1\.15/).pop(), 'string');
 	});
 
 });
