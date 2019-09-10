d6423bcdfa471d3c18633281857ff62cd36936a5
Rosano Coutinho <pub@xeari.com>
2019-09-09 21:07:57 -0400

    Tidy

--- os-app/open-vitrine/ui-test-feature.js
+++ os-app/open-vitrine/ui-test-feature.js
@@ -1,13 +1,13 @@
 import { deepEqual } from 'assert';
 
-const kDefaultRoutePath = require('./controller.js').OLSKControllerRoutes().LCHVitrineRoute.OLSKRoutePath;
+const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().LCHVitrineRoute;
 
 describe('LCHVitrineUIFeature', function () {
 
 context('LCHVitrineDemoButtonCommit', function testLCHVitrineDemoButtonCommit () {
 
 	before(function() {
-		return browser.visit(kDefaultRoutePath);
+		return browser.visit(kDefaultRoutePath.OLSKRoutePath);
 	});
 	
 	it('shows launcher', async function() {
