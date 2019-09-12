d0d7c0d7c15fa981af3465ee67c8f1287c7370f6
Rosano Coutinho <pub@xeari.com>
2019-09-12 16:30:47 -0400

    Hotfix zombie for ui tests

--- package-postinstall.js
+++ package-postinstall.js
@@ -1,3 +1,22 @@
+//# ROCOHotfixZombieForUITests
+
+(function ROCOHotfixZombieForUITests() {
+	if (process.env.NODE_ENV === 'production') {
+		return;
+	}
+
+	let filePath = './node_modules/zombie/lib/document.js';
+	require('fs').writeFileSync(filePath, require('fs')
+		.readFileSync(filePath, 'utf8')
+		.replace(
+			'this.dispatchEvent(event);',
+			`this.dispatchEvent(event)
+			const handled = browser.emit('OLSKMessage', data);
+		  if (!handled)
+		      browser.log('Unhandled message("%s")');`)
+	);
+})();
+
 //# ROCOHotfixULIDForBrowserTesting
 
 (function ROCOHotfixULIDForBrowserTesting() {
