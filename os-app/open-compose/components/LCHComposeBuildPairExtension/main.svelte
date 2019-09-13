661490a812280d8f204a6d01f653f0a4963f5054
Rosano Coutinho <pub@xeari.com>
2019-09-13 08:18:51 -0400

    Tidy

--- os-app/open-compose/components/LCHComposeBuildPairExtension/main.svelte
+++ os-app/open-compose/components/LCHComposeBuildPairExtension/main.svelte
@@ -95,21 +95,19 @@ window.addEventListener('message', mod.MessageReceived, false);
 </script>
 
 <div class="LCHComposeBuildPairExtension">
-{#if mod._ValueFormIsVisible}
+{#if mod.ValueFormIsVisible()}
 	<textarea class="LCHBuildPairExtensionPublicKeyField" placeholder="{ OLSKLocalized('LCHBuildPairExtensionPublicKeyFieldLabel') }" bind:value={ BuildPairExtensionPublicKey } autofocus ></textarea>
 	<button class="LCHBuildPairExtensionSubmitButton" on:click={ mod.InterfaceSubmitButtonDidClick }>{ OLSKLocalized('LCHBuildPairExtensionSubmitButtonLabel') }</button>
 {/if}
 
-{#if !mod._ValueFormIsVisible}
+{#if !mod.ValueFormIsVisible()}
 	<button class="LCHBuildPairExtensionUnpairButton" on:click={ mod.InterfaceUnpairButtonDidClick }>{ OLSKLocalized('LCHBuildPairExtensionUnpairButtonText') }</button>
 {/if}
 
-{#if mod._ValueStatus === 'kStatusWaiting' }
-	<span class="LCHBuildPairExtensionStatusWaiting"></span>
+{#if mod.ValueStatus() === 'kStatusWaiting' }
 {/if}
 
-{#if mod._ValueStatus === 'kStatusFailed' }
-	<span class="LCHBuildPairExtensionStatusFailed"></span>
+{#if mod.ValueStatus() === 'kStatusFailed' }
 {/if}
 
 </div>
