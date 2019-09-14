fd0affc4fd07c2a29b198b1a2f86e5f8ee86d776
Rosano Coutinho <pub@xeari.com>
2019-09-14 16:12:24 -0400

    Spec date string callback

--- os-app/open-vitrine/recipes/LCHVitrineMinimalistDateString/main-tests.js
+++ os-app/open-vitrine/recipes/LCHVitrineMinimalistDateString/main-tests.js
@@ -2,12 +2,21 @@ import { throws, deepEqual } from 'assert';
 
 import * as mainModule from './main.js';
 
+describe('LCHVitrineMinimalistDateStringCallback', function testLCHVitrineMinimalistDateStringCallback() {
+
+	it('returns string', async function() {
+		deepEqual(mainModule.LCHVitrineMinimalistDateStringCallback(), (new Date()).toJSON().slice(0, 10).replace(/-/g, '.'));
+	});
+
+});
+
 describe('LCHVitrineMinimalistDateStringRecipe', function testLCHVitrineMinimalistDateStringRecipe() {
 
 	it('returns LCHRecipe', async function() {
 		deepEqual(mainModule.LCHVitrineMinimalistDateStringRecipe(), {
 			LCHRecipeCallback: mainModule.LCHVitrineMinimalistDateStringCallback,
 			LCHRecipeSignature: 'LCHVitrineMinimalistDateString',
+			LCHRecipeOutputType: 'String',
 		});
 	});
 
