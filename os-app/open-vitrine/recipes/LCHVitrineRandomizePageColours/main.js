bf372bfd10b023beccc4f0d4ab1ad370f35da609
Rosano Coutinho <pub@xeari.com>
2019-09-14 12:41:38 -0400

    Spec randomize colours

--- os-app/open-vitrine/recipes/LCHVitrineRandomizePageColours/main.js
+++ os-app/open-vitrine/recipes/LCHVitrineRandomizePageColours/main.js
@@ -1,6 +1,21 @@
 import { OLSK_TESTING_BEHAVIOUR } from 'OLSKTesting'
 
 export const LCHVitrineRandomizePageColoursCallback = function() {
+	let element = document.querySelector('style.LCHVitrineRandomizePageColours')
+	
+	if (!element) {
+		document.body.appendChild(element = document.createElement('style')).classList.add('LCHVitrineRandomizePageColours')
+	};
+
+	const random = Math.random() * 100
+
+	element.innerHTML = `
+	body {
+
+	--LCHVitrineBackground: hsl(0, 0%, ${ random }%);
+	--LCHVitrineForeground: hsl(0, 0%, ${ 100.0 - random }%);
+	}
+`
 };
 
 export const LCHVitrineRandomizePageColoursRecipe = function() {
--- os-app/open-vitrine/ui-test-demo-commit.js
+++ os-app/open-vitrine/ui-test-demo-commit.js
@@ -9,14 +9,16 @@ const uFilter = function (inputData) {
 
 describe('LCHVitrineDemoCommit', function () {
 
-	beforeEach(async function () {
-		await browser.visit(kDefaultRoute.OLSKRoutePath);
-		
-		browser.click(LCHVitrineDemoButtonCommit);
-		await browser.wait({element: LCHLauncherFilterInput});
+	before(function () {
+		return browser.visit(kDefaultRoute.OLSKRoutePath)
 	});
 
-	describe('LCHVitrineSendEmail', function () {
+	context('LCHVitrineSendEmail', function () {
+
+		before(async function () {
+			browser.click(LCHVitrineDemoButtonCommit);
+			await browser.wait({element: LCHLauncherFilterInput});
+		});
 		
 		it('opens mailto', async function() {
 			await uFilter('send');
@@ -24,6 +26,55 @@ describe('LCHVitrineDemoCommit', function () {
 			deepEqual(browser.OLSKAlert(function () {
 				browser.click('.LCHLauncherResultListItem');
 			}), 'mailto:')
+		});
+	
+	});
+
+	context('LCHVitrineRandomizePageColours', function () {
+
+		const elementQuery = 'style.LCHVitrineRandomizePageColours';
+
+		before(async function () {
+			browser.click(LCHVitrineDemoButtonCommit);
+			await browser.wait({element: LCHLauncherFilterInput});
+		});
+
+		let item, initialContent;
+		
+		it('adds style element', async function() {
+			await uFilter('random');
+
+			browser.click('.LCHLauncherResultListItem');
+			await browser.wait({element: elementQuery});
+
+			browser.assert.elements(elementQuery, 1)
+
+			item = browser.query(elementQuery)
+			initialContent = item.innerHTML;
+		});
+		
+		it('sets --LCHVitrineBackground', async function() {
+			deepEqual(initialContent.includes('--LCHVitrineBackground'), true)
+		});
+		
+		it('sets --LCHVitrineForeground', async function() {
+			deepEqual(initialContent.includes('--LCHVitrineForeground'), true)
+		});
+		
+		it('updates element', async function() {
+			browser.click(LCHVitrineDemoButtonCommit);
+			await browser.wait({element: LCHLauncherFilterInput});
+
+			await uFilter('random');
+
+			browser.click('.LCHLauncherResultListItem');
+			await browser.wait({element: elementQuery});
+
+			deepEqual(item, browser.query(elementQuery))
+		});
+		
+		it('updates declarations', async function() {
+			deepEqual(initialContent !== browser.query(elementQuery).innerHTML, true)
 		});
 	
 	});
