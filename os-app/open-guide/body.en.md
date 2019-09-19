cae90b990b459d900109b2c638227b4f105220b0
Rosano Coutinho <pub@xeari.com>
2019-09-19 18:36:41 -0400

    Draft guide

--- os-app/open-guide/body.en.md
+++ os-app/open-guide/body.en.md
@@ -1,1 +1,75 @@
+# Launchlet Guide
 
+<div class="LCHGuideNotice">
+
+This document is a work-in-progress. Feel free to ask questions on [Stack Overflow](https://stackoverflow.com) or [Super User](https://superuser.com/) tagged with *launchlet*.
+
+</div>
+
+## Recipes: A Thinking Tutorial
+
+A *Recipe* is a format for expressing how functions will run in Launchlet.
+
+### Command
+
+A *Command* is the simplest kind of *Recipe*. It has a `LCHDocumentName` and `LCHDocumentBody`:
+
+LCHGuideTokenExampleCommandV1
+
+This can be run by Launchlet directly as it has no input.
+
+### Procedure
+
+A *Procedure* has a `LCHDocumentSignature` and `LCHDocumentBody`, and optionally `LCHDocumentArgs`:
+
+LCHGuideTokenExampleProcedure
+
+This can be called from any other *Recipe* using the API `this.api['Greet']` or `this.api.Greet`. In this way, the previous example could be changed to:
+
+LCHGuideTokenExampleCommandV2
+
+### Pipe mode: Subject
+
+A *Subject* has a `LCHDocumentName`, `LCHDocumentOutputType` and `LCHDocumentBody`:
+
+LCHGuideTokenExampleSubject
+
+In Pipe mode, this can be used as input for any *Action* that takes a `String`.
+
+### Pipe mode: Action
+
+An *Action* has a `LCHDocumentName`, `LCHDocumentInputTypes` and `LCHDocumentBody`:
+
+LCHGuideTokenExampleAction
+
+In Pipe mode, this can be used on any *Subject* that returns a `String`. Although a *Recipe* can have multiple inputs, an *Action* will generally have only one.
+
+* * *
+
+## Safety
+
+Running arbitrary JavaScript can be dangerous, and this project comes with no guarantees of safety from malicious code, but there are a few things that the composer will try to flag and ignore:
+- dynamic code such as `eval` or `new Function()`
+- stateful code that calls `document.cookie`, `localStorage`, `sessionStorage`, `XMLHttpRequest`, `fetch`
+
+Anyone with security expertise and ideas on how to improve safety in the project is welcome to add an Issue on [GitHub].
+
+It is best to run code that is concise and easy to read.
+
+* * *
+
+## Recipes from the current page
+
+Optionally, Launchlet can include Recipes from any page that sets `LCHPageRecipes` on the `window` object to an `Array`. This allows pages to expose their functionality in a way that is accessible to 3rd parties.
+
+For example, when running Launchlet on <a href="https://rosano.ca" target="_blank">rosano.ca</a>, it is possible to choose a 'Send a message' *Command* that is only accessible from that page.
+
+* * *
+
+## What is remoteStorage?
+
+[remoteStorage](https://remotestorage.io) is an incredible open-source technology for synchronizing data between multiple devices and making it available offline.
+
+It allows the same data to be used in different ways by multiple apps. You could think of it like a USB key with your documents that you can plug into websites to work on your stuff.
+
+If you don't have one, it also works with Dropbox and Google Drive. You can also get one for free from [5apps](https://5apps.com/storage/).
--- os-app/open-guide/controller.js
+++ os-app/open-guide/controller.js
@@ -2,44 +2,61 @@ exports.OLSKControllerUseLivereload = function() {
 	return process.env.NODE_ENV === 'development';
 };
 
+function OLSKRouteFunction (req, res, next) {
+	const { LCHGuideExampleFormatted, LCHGuideExampleQuotes } = require('./ui-logic.js');
+	function LCHGuideExampleTemplate(inputData) {
+		return `<div class="LCHGuideExample">\n\n${ LCHGuideExampleQuotes((function (inputData) {
+			let outputData = {};
+
+			for (key in inputData) {
+				outputData[res.locals.OLSKLocalized('LCHGuideDocumentFields')[key]] = inputData[key];
+			}
+
+			return outputData;
+		})(LCHGuideExampleFormatted(inputData))).join('\n\n') }\n\n</div>`;
+	};
+
+	return res.render(require('path').join(__dirname, 'ui-view'), {
+		LCHGuideContent: require('marked').setOptions({
+			gfm: true,
+		})(require('OLSKString').OLSKStringReplaceTokens(
+			require('fs').readFileSync(require('path').join(__dirname, `body.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8'),
+			Object.assign({
+				LCHGuideTokenExampleCommandV1: LCHGuideExampleTemplate({
+					LCHDocumentName: 'Say Hello',
+					LCHDocumentBody: `alert('Hello')`,
+				}),
+				LCHGuideTokenExampleProcedure: LCHGuideExampleTemplate({
+					LCHDocumentSignature: 'Greet',
+					LCHDocumentArgs: 'message',
+					LCHDocumentBody: `alert(message)`,
+				}),
+				LCHGuideTokenExampleCommandV2: LCHGuideExampleTemplate({
+					LCHDocumentName: 'Say Hello',
+					LCHDocumentBody: `this.api.Greet('Hello')`,
+				}),
+				LCHGuideTokenExampleSubject: LCHGuideExampleTemplate({
+					LCHDocumentName: 'Salutation',
+					LCHDocumentOutputType: 'String',
+					LCHDocumentBody: `return 'Yo'`,
+				}),
+				LCHGuideTokenExampleAction: LCHGuideExampleTemplate({
+					LCHDocumentName: 'Shout',
+					LCHDocumentInputTypes: 'String',
+					LCHDocumentArgs: 'message',
+					LCHDocumentBody: 'alert(message.toUppercase())',
+				}),
+			}, res.locals.OLSKLocalized('LCHGuideDocumentFields'))
+			)),
+		OLSKStringReplaceTokens: require('OLSKString').OLSKStringReplaceTokens,
+	});
+};
+
 exports.OLSKControllerRoutes = function() {
 	return [{
 		OLSKRoutePath: '/guide',
-		OLKSRouteSignature: 'LCHGuideRoute',
+		OLSKRouteSignature: 'LCHGuideRoute',
 		OLSKRouteMethod: 'get',
-		OLSKRouteFunction: function (req, res, next) {
-			const { LCHGuideExampleFormatted, LCHGuideExampleQuotes } = require('./ui-logic.js');
-
-			function LCHGuideExampleTemplate(inputData) {
-				return `<div class="LCHGuideExample">
-
-${ LCHGuideExampleQuotes((function (inputData) {
-	let outputData = {};
-
-	for (key in inputData) {
-		outputData[res.locals.OLSKLocalized('LCHGuideDocumentFields')[key]] = inputData[key];
-	}
-
-	return outputData
-})(LCHGuideExampleFormatted(inputData))).join('\n\n') }
-
-</div>`;
-			};
-
-			return res.render(require('path').join(__dirname, 'ui-view'), {
-				LCHGuideContent: require('marked').setOptions({
-					gfm: true,
-				})(require('OLSKString').OLSKStringReplaceTokens(
-					require('fs').readFileSync(require('path').join(__dirname, `body.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8'),
-					{
-						LCHGuideTokenExampleCommand: LCHGuideExampleTemplate({
-							LCHDocumentName: 'Say Hello',
-							LCHDocumentBody: `alert('Hello')`,
-						}),
-					},
-					)),
-				OLSKStringReplaceTokens: require('OLSKString').OLSKStringReplaceTokens,
-			});
-		},
+		OLSKRouteFunction,
 	}];
 };
