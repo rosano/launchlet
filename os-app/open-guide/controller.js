2f63942d75742f584d35aea65741aab6d38b26e2
Rosano Coutinho <pub@xeari.com>
2019-09-19 16:54:17 -0400

    Extract LCHGuideExampleTemplate

--- os-app/open-guide/controller.js
+++ os-app/open-guide/controller.js
@@ -8,9 +8,36 @@ exports.OLSKControllerRoutes = function() {
 		OLKSRouteSignature: 'LCHGuideRoute',
 		OLSKRouteMethod: 'get',
 		OLSKRouteFunction: function (req, res, next) {
+			const { LCHGuideExampleFormatted, LCHGuideExampleQuotes } = require('./ui-logic.js');
+
+			function LCHGuideExampleTemplate(inputData) {
+				return `<div class="LCHGuideExample">
+
+${ LCHGuideExampleQuotes((function (inputData) {
+	let outputData = {};
+
+	for (key in inputData) {
+		outputData[res.locals.OLSKLocalized('LCHGuideDocumentFields')[key]] = inputData[key];
+	}
+
+	return outputData
+})(LCHGuideExampleFormatted(inputData))).join('\n\n') }
+
+</div>`;
+			};
+
 			return res.render(require('path').join(__dirname, 'ui-view'), {
-				LCHGuideContent: require('OLSKString').OLSKStringReplaceTokens(require('marked')(require('fs').readFileSync(require('path').join(__dirname, `body.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8')), {
-				}),
+				LCHGuideContent: require('marked').setOptions({
+					gfm: true,
+				})(require('OLSKString').OLSKStringReplaceTokens(
+					require('fs').readFileSync(require('path').join(__dirname, `body.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8'),
+					{
+						LCHGuideTokenExampleCommand: LCHGuideExampleTemplate({
+							LCHDocumentName: 'Say Hello',
+							LCHDocumentBody: `alert('Hello')`,
+						}),
+					},
+					)),
 				OLSKStringReplaceTokens: require('OLSKString').OLSKStringReplaceTokens,
 			});
 		},
