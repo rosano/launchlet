b6b45f70a5434b79f9176b40b00025261673bf97
Rosano Coutinho <pub@xeari.com>
2019-09-26 15:56:31 -0400

    Tidy commit misc

--- os-app/dev-launcher/ui-test-_misc.js
+++ os-app/dev-launcher/ui-test-_misc.js
@@ -2,43 +2,175 @@ import { deepEqual } from 'assert';
 
 const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
-describe('LCHLauncherFeature', function () {
+describe('LCHLauncherMiscCommit', function() {
 
-	context('LCHLauncherModeCommit', function () {
+before(function() {
+	return browser.visit(OLSKTestingCanonicalFor(kDefaultRoute.OLSKRoutePath, {
+		StubRecipes: uStubStringify(uStubTwoItems()),
+		runMode: 'kRunModeCommit',
+	}));
+});
 
+before(function() {
+	browser.assert.input('.TestRecipeOutput', '');	
+});
+
+it('assert callbacks count 0')
+
+it('shows no items', function() {
+	browser.assert.elements(LCHLauncherListItem, 0);
+});
+
+describe('filter', function () {
+	
+	context('match', function () {
+		
 		before(function() {
-			return browser.visit(OLSKTestingCanonicalFor(kDefaultRoute.OLSKRoutePath, {
-				runMode: 'kRunModeCommit',
-			}));
-		});
-		
-		it('shows no items if no filter', function() {
-			browser.assert.elements(LCHLauncherListItem, 0);
-		});
-		
-		it('shows no items if no match', async function() {
-			browser.fill(LCHLauncherFilterInput, 'alfabravo');
-			await browser.wait({element: LCHLauncherFilterInput});
-
-			browser.assert.elements(LCHLauncherListItem, 0);
-		});
-
-		it('shows items if filter and match', async function() {
 			browser.fill(LCHLauncherFilterInput, 'a');
-			await browser.wait({element: LCHLauncherListItem});
-			
-			browser.assert.elements(LCHLauncherListItem, 5);
+		});
+		
+		it('shows matching items', function() {
+			browser.assert.elements(LCHLauncherListItem, 2);
 		});
 
-		it('selects first item', async function() {
-			browser.assert.hasClass(browser.queryAll(LCHLauncherListItem)[0], 'LCHLauncherResultListItemSelected');
+		it('selects first item', function() {
+			browser.assert.text('.LCHLauncherResultListItemSelected', 'alfa');
 		});
 
-		it.skip('selects item on mouseover', async function() {
-			browser.fire(browser.queryAll(LCHLauncherListItem)[1], 'mouseover');
-			await browser.wait({element: LCHLauncherListItem});
+		it('assert callbacks count 0')
 
-			browser.assert.hasClass(browser.queryAll(LCHLauncherListItem)[1], 'LCHLauncherResultListItemSelected');
+		it('runs no callback', function() {
+			browser.assert.input('.TestRecipeOutput', '');	
+		});
+	
+	});
+
+	context('match update', function () {
+		
+		before(function() {
+			browser.fill(LCHLauncherFilterInput, 'al');
+		});
+		
+		it('shows matching items', function() {
+			browser.assert.elements(LCHLauncherListItem, 1);
+		});
+
+		it('assert callbacks count 0')
+	
+	});
+	
+	context('no match', function () {
+		
+		before(function() {
+			browser.fill(LCHLauncherFilterInput, 'alb');
+		});
+		
+		it('shows no items', function() {
+			browser.assert.elements(LCHLauncherListItem, 0);
+		});
+
+		it('assert callbacks count 0')
+	
+	});
+
+});
+
+describe('results', function () {
+
+	before(function() {
+		browser.fill(LCHLauncherFilterInput, 'a');
+	});
+
+	context('ArrowDown', function () {
+		
+		before(function () {
+			return browser.OLSKFireKeyboardEvent(browser.window, 'ArrowDown');
+		});
+
+		it('selects next item', function() {
+			browser.assert.text('.LCHLauncherResultListItemSelected', 'bravo');
+		});
+	
+	});
+
+	context('ArrowUp', function () {
+		
+		before(function () {
+			return browser.OLSKFireKeyboardEvent(browser.window, 'ArrowUp');
+		});
+
+		it('selects previous item', function() {
+			browser.assert.text('.LCHLauncherResultListItemSelected', 'alfa');
+		});
+	
+	});
+
+	it('sets class on mouseover')
+	context.skip('mouseover', function () {
+
+		before(function () {
+			return browser.fire(`${ LCHLauncherListItem }:nth-child(2)`, 'mouseover');
+		});
+		
+		it('sets class', function () {
+			browser.assert.text('.LCHLauncherResultListItemSelected', 'bravo');	
+		});
+	
+	});
+
+	context('click', function () {
+
+		before(function () {
+			browser.assert.input('.TestRecipeOutput', '');
+		});
+
+		before(function () {
+			return browser.fire(`${ LCHLauncherListItem }:nth-child(2)`, 'click');
+		});
+
+		it('assert callbacks count 1')
+		
+		it('runs callback', function () {
+			browser.assert.input('.TestRecipeOutput', 'bravo');	
+		});
+		
+		it('hides LCHLauncher', function () {
+			browser.assert.elements(LCHLauncher, 0);
+		});
+	
+	});
+
+	context('Enter', function () {
+
+		before(function () {
+			return browser.pressButton('.TestLauncherInvoke');
+		});
+		
+		before(function() {
+			browser.fill(LCHLauncherFilterInput, 'a');
+		});
+		
+		before(function() {
+			browser.OLSKFireKeyboardEvent(browser.window, 'Enter');
+		});
+
+		it('assert callbacks count 2')
+		
+		it('runs callback', function () {
+			browser.assert.input('.TestRecipeOutput', 'alfa');	
+		});
+		
+		it('hides LCHLauncher', function () {
+			browser.assert.elements(LCHLauncher, 0);
+		});
+	
+	});
+
+});
+	
+});
+
+
 		});
 
 		it('does not run item on select', async function() {
