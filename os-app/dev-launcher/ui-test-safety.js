b807da910a107afd5006c7557aef8f1b0707d774
Rosano Coutinho <pub@xeari.com>
2019-09-26 17:38:11 -0400

    Tidy launcher safety tests

--- os-app/dev-launcher/ui-test-safety.js
+++ os-app/dev-launcher/ui-test-safety.js
@@ -4,14 +4,60 @@ const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
 describe('LCHLauncherSafety', function () {
 
-	context('AutomaticRunningOfPageRecipes', function testAutomaticRunningOfPageRecipes () {
+	describe('AutomaticRunningOfPageRecipes', function testAutomaticRunningOfPageRecipes () {
 
 		before(function() {
-			return browser.visit(kDefaultRoute.OLSKRoutePath);
+			return browser.visit(OLSKTestingCanonicalFor(kDefaultRoute.OLSKRoutePath, {
+				LCHOptionIncludePageRecipes: true,
+			}));
+		});
+
+		before(function () {
+			browser.click('.TestRecipeOutput')
+		});
+
+		before(function () {
+			browser.assert.elements(LCHLauncher, 0)
+			browser.assert.input('.TestRecipeOutput', '')
+		});
+
+		before(function () {
+			browser.evaluate(`window.LCHPageRecipes = [{
+				LCHRecipeName: 'alfa',
+				LCHRecipeCallback: function () {
+					document.querySelector('.TestRecipeOutput').value = 'bravo';
+				},
+				LCHRecipeURLFilter: '*',
+				LCHRecipeIsAutomatic: true,
+			}]`)
+		});
+
+		before(function () {
+			return browser.pressButton('.TestLauncherInvoke');
+		});
+
+		before(function () {
+			return browser.assert.input('.TestRecipeOutput', '')
 		});
 		
-		it('strips LCHRecipeIsAutomatic from LCHPageRecipes', async function() {
-			browser.assert.input('input', '');
+		it('strips LCHRecipeIsAutomatic from LCHPageRecipes', function() {
+			browser.assert.input('.TestRecipeOutput', '')
+		});
+
+		context('filter', function () {
+			
+			before(	function () {
+				return browser.fill(LCHLauncherFilterInput, 'alfa');
+			});
+
+			before(	function() {
+				return browser.click(LCHLauncherListItem);
+			});
+			
+			it('runs item', function() {
+				browser.assert.input('.TestRecipeOutput', 'bravo')
+			});
+		
 		});
 
 	});
