42adc06ea614c059cdb88881b5cf45f3bb173e98
Rosano Coutinho <pub@xeari.com>
2019-09-27 08:16:01 -0400

    Extract prompt tests

---
+++ os-app/dev-launcher/ui-test-prompt.js
@@ -0,0 +1,114 @@
+import { deepEqual } from 'assert';
+
+const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
+
+describe('LCHLauncherPrompt', function () {
+
+	before(function() {
+		return browser.visit(OLSKTestingCanonicalFor(kDefaultRoute.OLSKRoutePath, {
+			StubRecipes: uStubStringify(uStubTwoItems()),
+			runMode: 'kRunModePipe',
+		}));
+	});
+
+	it('selects first prompt', function() {
+		browser.assert.hasClass(LCHLauncherSubjectPrompt, 'LCHLauncherPromptSelected');
+	});
+
+	context('click', function () {
+
+		before(function() {
+			return browser.OLSKFireKeyboardEvent(browser.window, 'a');
+		});
+		
+		before(function() {
+			return browser.click(LCHLauncherActionPrompt);
+		});
+
+		it('selects prompt', function() {
+			browser.assert.hasClass(LCHLauncherActionPrompt, 'LCHLauncherPromptSelected');
+		});
+	
+	});
+
+	context('Tab', function () {
+
+		before(function() {
+			return browser.OLSKFireKeyboardEvent(browser.window, 'Tab');
+		});
+
+		it('selects next prompt', function() {
+			browser.assert.hasClass(LCHLauncherSubjectPrompt, 'LCHLauncherPromptSelected');
+		});
+	
+	});
+
+	context('Shift+Tab', function () {
+
+		before(function() {
+			return browser.OLSKFireKeyboardEvent(browser.window, 'Tab', {
+				shiftKey: true,
+			});
+		});
+
+		it('selects previous prompt', function() {
+			browser.assert.hasClass(LCHLauncherActionPrompt, 'LCHLauncherPromptSelected');
+		});
+	
+	});
+
+	context('no subject', function () {
+
+		before(function() {
+			browser.click(LCHLauncherSubjectPrompt);
+		});
+
+		before(function() {
+			return browser.OLSKFireKeyboardEvent(browser.window, 'Backspace');
+		});
+
+		before(function() {
+			return browser.OLSKFireKeyboardEvent(browser.window, 'Backspace');
+		});
+
+		context('click', function () {
+			
+			before(function() {
+				browser.click(LCHLauncherSubjectPrompt);
+			});
+
+			it('does nothing', function() {
+				browser.assert.hasClass(LCHLauncherSubjectPrompt, 'LCHLauncherPromptSelected');
+			});
+		
+		});
+
+		context('Tab', function () {
+
+			before(function() {
+				return browser.OLSKFireKeyboardEvent(browser.window, 'Tab');
+			});
+
+			it('does nothing', function() {
+				browser.assert.hasClass(LCHLauncherSubjectPrompt, 'LCHLauncherPromptSelected');
+			});
+		
+		});
+
+		context('Shift+Tab', function () {
+
+			before(function() {
+				return browser.OLSKFireKeyboardEvent(browser.window, 'Tab', {
+					shiftKey: true,
+				});
+			});
+
+			it('does nothing', function() {
+				browser.assert.hasClass(LCHLauncherSubjectPrompt, 'LCHLauncherPromptSelected');
+			});
+		
+		});
+	
+	});
+
+});
