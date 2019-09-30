b62ec0a2ae085a973f28b60b89be06a43bdf3640
Rosano Coutinho <pub@xeari.com>
2019-09-30 10:25:08 -0400

    Fix typo

--- os-app/open-compose/submodules/LCHComposeBuildPairExtension/ui-test-_localize.js
+++ os-app/open-compose/submodules/LCHComposeBuildPairExtension/ui-test-_localize.js
@@ -8,7 +8,7 @@ const uLocalized = function (inputData) {
 	return OLSKTestingLocalized(inputData, languageCode);
 };
 
-describe(`LCHCompose_Localize-${ languageCode }`, function () {
+describe(`LCHBuildPairExtension_Localize-${ languageCode }`, function () {
 
 	before(function() {
 		return browser.visit(OLSKTestingCanonicalFor(kDefaultRoute.OLSKRoutePath, {
