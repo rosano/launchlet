c5aabc3dc2d57238cbddda8c5022a00096d2e147
Rosano Coutinho <pub@xeari.com>
2019-10-02 18:35:25 -0400

    Pass tests

--- app/open-compose/submodules/LCHComposeBuild/stub-view.svelte
+++ app/open-compose/submodules/LCHComposeBuild/stub-view.svelte
@@ -3,6 +3,8 @@ import Module from './main.svelte';
 </script>
 
 <Module
+	LCHComposeBuildPackageStyle={ '' }
+	LCHComposeBuildPackageScript={ '' }
 	BuildInitializeModePipeEnabled={ false }
 	BuildAppLanguageCode={ window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage') }
 	{...Object.fromEntries((new window.URLSearchParams(window.location.search)).entries())}
