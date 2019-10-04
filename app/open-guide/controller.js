6301a18b1a1b38ada556ceb56d3c675a672375b2
Rosano Coutinho <pub@xeari.com>
2019-10-04 18:09:36 -0400

    Configure marked

--- app/open-guide/controller.js
+++ app/open-guide/controller.js
@@ -13,6 +13,7 @@ function OLSKRouteFunction (req, res, next) {
 	return res.render(require('path').join(__dirname, 'ui-view'), {
 		LCHGuideContent: require('marked').setOptions({
 			gfm: true,
+			headerIds: false,
 		})(require('OLSKString').OLSKStringReplaceTokens(
 			require('fs').readFileSync(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8'),
 			Object.assign({
--- app/open-vitrine/controller.js
+++ app/open-vitrine/controller.js
@@ -17,7 +17,10 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteSignature: 'LCHVitrineRoute',
 		OLSKRouteFunction: function (req, res, next) {
 			return res.render(require('path').join(__dirname, 'ui-view'), {
-				LCHVitrineContent: require('OLSKString').OLSKStringReplaceTokens(require('marked')(require('fs').readFileSync(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8')), Object.assign({
+				LCHVitrineContent: require('OLSKString').OLSKStringReplaceTokens(require('marked').setOptions({
+					gfm: true,
+					headerIds: false,
+				})(require('fs').readFileSync(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8')), Object.assign({
 					LCHVitrineTokenComposeURL: res.locals.OLSKCanonicalLocalizedFor('LCHComposeRoute'),
 					LCHVitrineTokenGuideURL: res.locals.OLSKCanonicalFor('LCHGuideRoute'),
 					LCH_VITRINE_QUICKSILVER_URL: process.env.LCH_VITRINE_QUICKSILVER_URL,
