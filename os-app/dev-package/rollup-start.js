9f789f117be77bcd8ceb343f1d6363a242ff1f6e
Rosano Coutinho <pub@xeari.com>
2019-11-07 11:31:06 -0500

    Fix typo

--- os-app/dev-package/rollup-start.js
+++ os-app/dev-package/rollup-start.js
@@ -1,4 +1,4 @@
-const mainModule = require('./main.js');
+import * as mainModule from './main.js';
 
 import Main from '../dev-launcher/__compiled/ui-behaviour.js';
 mainModule.mod._ValueClass = Main;
