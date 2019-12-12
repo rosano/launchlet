036b6023f691b9553f3f758055973468b0c45b48
Rosano Coutinho <pub@xeari.com>
2019-12-12 18:20:14 -0500

    Tidy response render

--- os-app/dev-launcher/controller.js
+++ os-app/dev-launcher/controller.js
@@ -4,7 +4,7 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteMethod: 'get',
 		OLSKRouteSignature: 'LCHLauncherRoute',
 		OLSKRouteFunction: function(req, res, next) {
-			return res.render(require('path').join(__dirname, 'ui-view'), {});
+			return res.render(require('path').join(__dirname, 'ui-view'));
 		},
 		OLSKRouteLanguages: ['en', 'fr', 'es'],
 		OLSKRouteIsHidden: process.env.NODE_ENV === 'production',
--- os-app/dev-package/controller.js
+++ os-app/dev-package/controller.js
@@ -6,7 +6,7 @@ exports.OLSKControllerRoutes = function() {
 			OLSKRoutePath: '/package',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction: function(req, res, next) {
-				return res.render(require('path').join(__dirname, 'ui-view'), {});
+				return res.render(require('path').join(__dirname, 'ui-view'));
 			},
 			OLSKRouteLanguages: ['en'],
 			OLSKRouteIsHidden: process.env.NODE_ENV === 'production',
