e78635be5b2037131136989162e7cd2cc738445c
Rosano Coutinho <pub@xeari.com>
2020-07-11 10:21:23 -0300

    Tidy document model

--- os-app/_shared/LCHDocument/model.js
+++ os-app/_shared/LCHDocument/model.js
@@ -1,63 +1,69 @@
 import { LCHFormulaModelErrorsFor, LCHFormulaFrom, LCHFormulaTo } from '../LCHFormula/main.js';
 
-export const LCHDocumentModelErrorsFor = function(inputData, options = {}) {
-	if (typeof inputData !== 'object' || inputData === null) {
-		throw new Error('LCHErrorInputNotValid');
-	}
+const mod = {
 
-	var errors = LCHFormulaTo(LCHFormulaModelErrorsFor(LCHFormulaFrom(inputData)) || {}, 'LCHDocument');
+	LCHDocumentModelErrorsFor (inputData, options = {}) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			throw new Error('LCHErrorInputNotValid');
+		}
 
-	if (typeof inputData.LCHDocumentID !== 'string') {
-		errors.LCHDocumentID = [
-			'LCHErrorNotString',
-		];
-	} else if (inputData.LCHDocumentID.trim() === '') {
-		errors.LCHDocumentID = [
-			'LCHErrorNotFilled',
-		];
-	}
+		var errors = LCHFormulaTo(LCHFormulaModelErrorsFor(LCHFormulaFrom(inputData)) || {}, 'LCHDocument');
 
-	if (typeof inputData.LCHDocumentCallbackBody !== 'string') {
-		errors.LCHDocumentCallbackBody = [
-			'LCHErrorNotString',
-		];
-	}
+		if (typeof inputData.LCHDocumentID !== 'string') {
+			errors.LCHDocumentID = [
+				'LCHErrorNotString',
+			];
+		} else if (inputData.LCHDocumentID.trim() === '') {
+			errors.LCHDocumentID = [
+				'LCHErrorNotFilled',
+			];
+		}
 
-	if (!(inputData.LCHDocumentCreationDate instanceof Date) || Number.isNaN(inputData.LCHDocumentCreationDate.getTime())) {
-		errors.LCHDocumentCreationDate = [
-			'LCHErrorNotDate',
-		];
-	}
-
-	if (!(inputData.LCHDocumentModificationDate instanceof Date) || Number.isNaN(inputData.LCHDocumentModificationDate.getTime())) {
-		errors.LCHDocumentModificationDate = [
-			'LCHErrorNotDate',
-		];
-	}
-
-	if (inputData.LCHDocumentCallbackArgs !== undefined || options.LCHOptionValidateIfNotPresent) {
-		if (typeof inputData.LCHDocumentCallbackArgs !== 'string') {
-			errors.LCHDocumentCallbackArgs = [
+		if (typeof inputData.LCHDocumentCallbackBody !== 'string') {
+			errors.LCHDocumentCallbackBody = [
 				'LCHErrorNotString',
 			];
 		}
-	}
 
-	if (inputData.LCHDocumentSyntaxErrorMessage !== undefined || options.LCHOptionValidateIfNotPresent) {
-		if (typeof inputData.LCHDocumentSyntaxErrorMessage !== 'string') {
-			errors.LCHDocumentSyntaxErrorMessage = [
-				'LCHErrorNotString',
+		if (!(inputData.LCHDocumentCreationDate instanceof Date) || Number.isNaN(inputData.LCHDocumentCreationDate.getTime())) {
+			errors.LCHDocumentCreationDate = [
+				'LCHErrorNotDate',
 			];
 		}
-	}
 
-	if (inputData.LCHDocumentCanonicalExampleCallbackBody !== undefined || options.LCHOptionValidateIfNotPresent) {
-		if (typeof inputData.LCHDocumentCanonicalExampleCallbackBody !== 'string') {
-			errors.LCHDocumentCanonicalExampleCallbackBody = [
-				'LCHErrorNotString',
+		if (!(inputData.LCHDocumentModificationDate instanceof Date) || Number.isNaN(inputData.LCHDocumentModificationDate.getTime())) {
+			errors.LCHDocumentModificationDate = [
+				'LCHErrorNotDate',
 			];
 		}
-	}
 
-	return Object.entries(errors).length ? errors : null;
+		if (inputData.LCHDocumentCallbackArgs !== undefined || options.LCHOptionValidateIfNotPresent) {
+			if (typeof inputData.LCHDocumentCallbackArgs !== 'string') {
+				errors.LCHDocumentCallbackArgs = [
+					'LCHErrorNotString',
+				];
+			}
+		}
+
+		if (inputData.LCHDocumentSyntaxErrorMessage !== undefined || options.LCHOptionValidateIfNotPresent) {
+			if (typeof inputData.LCHDocumentSyntaxErrorMessage !== 'string') {
+				errors.LCHDocumentSyntaxErrorMessage = [
+					'LCHErrorNotString',
+				];
+			}
+		}
+
+		if (inputData.LCHDocumentCanonicalExampleCallbackBody !== undefined || options.LCHOptionValidateIfNotPresent) {
+			if (typeof inputData.LCHDocumentCanonicalExampleCallbackBody !== 'string') {
+				errors.LCHDocumentCanonicalExampleCallbackBody = [
+					'LCHErrorNotString',
+				];
+			}
+		}
+
+		return Object.entries(errors).length ? errors : null;
+	},
+
 };
+
+export default mod;
--- os-app/_shared/LCHDocument/storage.js
+++ os-app/_shared/LCHDocument/storage.js
@@ -1,4 +1,4 @@
-import * as LCHDocumentModel from './model.js';
+import LCHDocumentModel from './model.js';
 import * as OLSKRemoteStoragePackage from 'OLSKRemoteStorage';
 const OLSKRemoteStorage = OLSKRemoteStoragePackage.default || OLSKRemoteStoragePackage;
 
