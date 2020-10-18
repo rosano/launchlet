e9bcc4fd26169bd17590daf4c03ed1a85bc4a166
Rosano Coutinho <pub@xeari.com>
2020-10-18 11:28:45 -0300

    Tidy

--- os-app/dev-launcher/recipes/functions/LCHReadTextFile/main.js
+++ os-app/dev-launcher/recipes/functions/LCHReadTextFile/main.js
@@ -1,15 +1,13 @@
 export const LCHReadTextFileCallback = async function(inputData = {}) {
 	return new Promise(function (res, rej) {
-		Object.assign(document.createElement('input'), inputData, {
+		return Object.assign(document.createElement('input'), inputData, {
 			type: 'file',
-			onchange (e) {
-				const fileReader = new FileReader();
-				
-				fileReader.onload = function (event) {
-					return res(event.target.result);
-				};
-
-				fileReader.readAsText(e.target.files[0]);
+			onchange (event) {
+				return Object.assign(new FileReader(), {
+					onload (event) {
+						return res(event.target.result);
+					},
+				}).readAsText(event.target.files[0]);
 			},
 		}).click();
 	});
