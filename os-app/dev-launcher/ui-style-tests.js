23b4a807c8128ee83394ef083974fccebd506e90
Rosano Coutinho <pub@xeari.com>
2020-12-04 14:00:22 -0300

    Fix typo

--- os-app/dev-launcher/ui-style-tests.js
+++ os-app/dev-launcher/ui-style-tests.js
@@ -1,13 +1,15 @@
+const { deepEqual } = require('assert');
+
 const inputData = require('fs').readFileSync(require('path').join(__dirname, '__compiled/ui-style.css'), 'utf8');
 
 describe('LCHLauncherStyle', function test_LCHLauncherStyle() {
 
 	it('contains normalize', function() {
-		browser.assert.deepEqual(inputData.match(/\{(.*00%)\}/)[1], 'line-height:1.15;-webkit-text-size-adjust:100%');
+		deepEqual(inputData.match(/\{(.*00%)\}/)[1], 'line-height:1.15;-webkit-text-size-adjust:100%');
 	});
 
 	it('prefixes normalize', function() {
-		browser.assert.deepEqual(typeof inputData.match(/\.Container\.svelte-\w+\{line-height:1\.15/).pop(), 'string');
+		deepEqual(typeof inputData.match(/\.Container\.svelte-\w+\{line-height:1\.15/).pop(), 'string');
 	});
 
 });
--- os-app/dev-package/ui-style-tests.js
+++ os-app/dev-package/ui-style-tests.js
@@ -1,7 +1,9 @@
+const { deepEqual } = require('assert');
+
 describe('LCHPackageStyle', function test_LCHPackageStyle() {
 
 	it('contains launcher style', function() {
-		browser.assert.deepEqual(require('fs').readFileSync(require('path').join(__dirname, '__compiled/launchlet.css'), 'utf8').slice(0, 100), require('fs').readFileSync(require('path').join(__dirname, '../dev-launcher/__compiled/ui-style.css'), 'utf8').slice(0, 100));
+		deepEqual(require('fs').readFileSync(require('path').join(__dirname, '__compiled/launchlet.css'), 'utf8').slice(0, 100), require('fs').readFileSync(require('path').join(__dirname, '../dev-launcher/__compiled/ui-style.css'), 'utf8').slice(0, 100));
 	});
 
 });
