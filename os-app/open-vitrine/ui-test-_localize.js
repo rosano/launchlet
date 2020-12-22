rosano/kommit

Propagate Support

os-app/open-vitrine/ui-test-_localize.js
@@ -65,6 +65,14 @@ kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
 			browser.assert.text(KOMVitrineVideoHeading, uLocalized('OLSKWordingVideo'));
 		});
 
+		it('localizes KOMVitrineSupportHeading', function () {
+			browser.assert.text(KOMVitrineSupportHeading, uLocalized('OLSKWordingSupportHeading'));
+		});
+
+		it('localizes KOMVitrineSupportBlurb', function () {
+			browser.assert.text(KOMVitrineSupportBlurb, uLocalized('OLSKWordingSupportBlurb'));
+		});
+
 		context('KOMVitrineContentAppButton', function test_KOMVitrineContentAppButton () {
 
 			it('classes OLSKCommonButton', function () {

rosano/launchlet

os-app/open-vitrine/ui-test-_localize.js
@@ -69,6 +69,14 @@ describe(`LCHVitrine_Localize-${ OLSKRoutingLanguage }`, function () {
 		browser.assert.text(LCHVitrineVideoHeading, uLocalized('OLSKWordingVideo'));
 	});
 
+	it('localizes LCHVitrineSupportHeading', function () {
+		browser.assert.text(LCHVitrineSupportHeading, uLocalized('OLSKWordingSupportHeading'));
+	});
+
+	it('localizes LCHVitrineSupportBlurb', function () {
+		browser.assert.text(LCHVitrineSupportBlurb, uLocalized('OLSKWordingSupportBlurb'));
+	});
+
 	it('localizes LCHVitrineDemoButtonCommit', function() {
 		browser.assert.text(LCHVitrineDemoButtonCommit, uLocalized('LCHVitrineDemoButtonCommitText'))
 	});