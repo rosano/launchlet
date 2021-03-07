85e471b7a7a94ca251c0783046458c45f886bf94
Rosano Coutinho <pub@xeari.com>
2021-03-07 10:28:47 -0300

    Fix typo

--- os-app/open-compose/main.svelte
+++ os-app/open-compose/main.svelte
@@ -1141,7 +1141,7 @@ const mod = {
 		if (zerodatawrap.ZDRPreferenceProtocolMigrate()) {
 			const client = await mod.DataStorageClient(zerodatawrap.ZDRPreferenceProtocolMigrate());
 
-			await Promise.all((await client.App.ZDRStoragePathsRecursive('')).map(async function (e) {
+			await Promise.all((await client.App.ZDRStoragePathsRecursive('/')).map(async function (e) {
 				await mod._ValueZDRWrap.App.ZDRStorageWriteObject(e, await client.App.ZDRStorageReadObject(e));
 				await client.App.ZDRStorageDeleteFile(e);
 			}));
