838933df231e2376e5f1fd74a5f800f68ab6aced
Rosano Coutinho <pub@xeari.com>
2021-07-26 10:12:52 -0300

    Move OLSKEdit after OLSKJar

--- os-app/open-vitrine/ui-test-_access.js
+++ os-app/open-vitrine/ui-test-_access.js
@@ -109,16 +109,16 @@ describe('LCHVitrine_Access', function () {
 		browser.assert.elements(LCHVitrineTutorialsButton, 1);
 	});
 
-	it('shows OLSKEdit', function () {
-		browser.assert.elements('.OLSKEdit', 1);
-	});
-
 	it('shows ROCOGazette', function () {
 		browser.assert.elements('.ROCOGazette', 1);
 	});
 
 	it('shows OLSKJar', function () {
 		browser.assert.elements('.OLSKJar', 1);
+	});
+
+	it('shows OLSKEdit', function () {
+		browser.assert.elements('.OLSKEdit', 1);
 	});
 
 	it('shows ROCOForum', function () {
