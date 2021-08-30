eafd211df48310d1e39d71a4079351fdddaef9ee
Rosano Coutinho <pub@xeari.com>
2021-08-30 11:10:44 -0400

    Purge identity redirect

--- os-app/_shared/common/controller.js
+++ os-app/_shared/common/controller.js
@@ -1,20 +1,3 @@
-const pathPackage = require('path');
-
-exports.OLSKControllerRoutes = function() {
-	return {
-		LCHCommonIdentityRedirect: {
-			OLSKRoutePath: '/identity.svg',
-			OLSKRouteMethod: 'get',
-			OLSKRouteRedirect: '/_shared/LCHRootLink/ui-assets/identity.svg',
-		},
-		LCHCommonLogoRedirect: {
-			OLSKRoutePath: '/logo.svg',
-			OLSKRouteMethod: 'get',
-			OLSKRouteRedirect: '/identity.svg',
-		},
-	};
-};
-
 exports.OLSKControllerSharedStaticAssetFolders = function() {
 	return [
 		'_shared/__external',
